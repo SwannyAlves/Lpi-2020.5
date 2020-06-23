bcf7e36f2c7ddd92af1e4764deb4a498dd3e5e07
SwannyAlves <swannykathllen@gmail.com>
2020-06-23 16:28:27 -0300

    aula 6

---
+++ src/Diary.cpp
@@ -0,0 +1,16 @@
+#include "Diary.h"
+
+//para inicializar variaveis da struct coloca ":nome_da_variavel(inicializa)"
+Diary::Diary(const std::string& filename) : messages(), filename()
+{
+}
+
+void Diary::add(const std::string& message) 
+{
+    // adicionar mensagem no array de mensagens
+}
+
+void Diary::write()
+{
+    // gravar as mensagens no disco
+}
